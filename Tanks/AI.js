b3e2aa783239cd9b3c883184c7fe56d8072b15fe
DanniGudna <dannigudna@gmail.com>
2017-11-15 16:02:03 +0000

    breytti entity manager runu í ship

--- Tanks/AI.js
+++ Tanks/AI.js
@@ -4,19 +4,21 @@
 
 var ai = {
 
+  timer:  1000,
 
-  runAi: function(destX, startVelX, direction, path){
+  runAI: function(destX, startVelX, direction, path,){
     //fær inn öll gildi frá ships sem þarf og kallar svo á hin föllinn með þeim
     //þá þarf bara eitt kall í ships
     //implementa seinast
     //timer?
-    Aiupdate(destX, startVelX, direction, path);
+    //óþarfi
+    AIupdate(destX, startVelX, direction, path, timer);
 
   },
 
-  getTarget: function(){
+  getTarget: function(ship){
 
-    var targetx = entityManager._ships[gameplayManager.activePlayerIndex].playerNr + 1;
+    var targetx = ship.playerNr + 1;
     targetx %= entityManager._ships.length;
     //AI will not aim at a dead player
     //will get fixed once we have a winner screen
@@ -28,7 +30,7 @@ var ai = {
     return entityManager._ships[targetx].cx
   },
 
-  AIMovement: function(AIpath){
+  AIMovement: function(AIpath, ship){
     /*movement of the AI */
     var thrust;
     if(AIpath === 0){
@@ -41,32 +43,32 @@ var ai = {
 
     } else if(AIpath < 0){
 
-      entityManager._ships[gameplayManager.activePlayerIndex].updateRotation();
+      ship.updateRotation();
       thrust = -1;
 
-      if ((entityManager._ships[gameplayManager.activePlayerIndex].rotation < -50) || (entityManager._ships[gameplayManager.activePlayerIndex].rotation > 50)) {
+      if ((ship.rotation < -50) || (ship.rotation > 50)) {
 
-        thrust = entityManager._ships[gameplayManager.activePlayerIndex].falldown(thrust);
+        thrust = ship.falldown(thrust);
       }
 
-      if( entityManager._ships[gameplayManager.activePlayerIndex].cx - entityManager._ships[gameplayManager.activePlayerIndex].sprite.width / 2 + 10 > 0){
-      entityManager._ships[gameplayManager.activePlayerIndex].applyAccel(thrust);
+      if( ship.cx - ship.sprite.width / 2 + 10 > 0){
+      ship.applyAccel(thrust);
           AIpath++;
     } else { AIpath = 150}
 
 
     } else if(AIpath > 0){
 
-      entityManager._ships[gameplayManager.activePlayerIndex].updateRotation();
+      ship.updateRotation();
       thrust = 1;
 
-      if ((entityManager._ships[gameplayManager.activePlayerIndex].rotation < -50) || (entityManager._ships[gameplayManager.activePlayerIndex].rotation > 50)) {
+      if ((ship.rotation < -50) || (ship.rotation > 50)) {
 
-        thrust = entityManager._ships[gameplayManager.activePlayerIndex].falldown(thrust);
+        thrust = ship.falldown(thrust);
       }
 
-      if(entityManager._ships[gameplayManager.activePlayerIndex].cx + entityManager._ships[gameplayManager.activePlayerIndex].sprite.width / 2 < g_canvas.width){
-      entityManager._ships[gameplayManager.activePlayerIndex].applyAccel(thrust);
+      if(ship.cx + ship.sprite.width / 2 < g_canvas.width){
+      ship.applyAccel(thrust);
       AIpath--;
     }else { AIpath = -150}
 
@@ -75,70 +77,76 @@ var ai = {
     return AIpath;
   },
 
-  AIrotation: function(AIdirection){
+  AIrotation: function(AIdirection, ship){
 
     /*Rotation of the AI gun*/
-    if (Math.floor(util.toDegrees(entityManager._ships[gameplayManager.activePlayerIndex].gunrotation)) >= 180) {
+    if (Math.floor(util.toDegrees(ship.gunrotation)) >= 180) {
       AIdirection = "left";
     }
 
-    if (Math.floor(util.toDegrees(entityManager._ships[gameplayManager.activePlayerIndex].gunrotation)) <= 0) {
+    if (Math.floor(util.toDegrees(ship.gunrotation)) <= 0) {
 
       AIdirection = "right";
     }
 
     if (AIdirection === "left") {
-      entityManager._ships[gameplayManager.activePlayerIndex].gunrotation -= NOMINAL_ROTATE_RATE * 2;
+      ship.gunrotation -= NOMINAL_ROTATE_RATE * 2;
     }
 
     if (AIdirection === "right") {
-      entityManager._ships[gameplayManager.activePlayerIndex].gunrotation += NOMINAL_ROTATE_RATE * 2;
+      ship.gunrotation += NOMINAL_ROTATE_RATE * 2;
     }
 
-      entityManager._ships[gameplayManager.activePlayerIndex].spriteGunRotation = util.toDegrees(entityManager._ships[gameplayManager.activePlayerIndex].gunrotation) - 90;
-      entityManager._ships[gameplayManager.activePlayerIndex].spriteGunRotation += entityManager._ships[gameplayManager.activePlayerIndex].rotation;
+      ship.spriteGunRotation = util.toDegrees(ship.gunrotation) - 90;
+      ship.spriteGunRotation += ship.rotation;
       return AIdirection;
   },
 
-  AIupdate: function (destX, startVelX, direction, path, timer){
+  AIupdate: function (destX, startVelX, direction, path){
+    var ship = entityManager._ships[gameplayManager.activePlayerIndex];
 
-    var targetx = this.getTarget();
+    var targetx = this.getTarget(ship);
+    if(this.timer < 0){
+      console.log("ping")
+      ship.maybeFireBullet();
+      this.timer = 1000;
+    }
+    this.timer--;
 
-    //if (this.playerId === "AI") {
-    //  if (this.myTurn === true) {
         if (Math.floor(destX) < targetx && targetx - 20 < Math.floor(destX) || Math.floor(destX) < targetx && targetx + 20 < Math.floor(destX)) {
           //&& targetx - this.cx > 50 || this.cx - targetx > 50
 
           //console.log(Math.abs(targetx - entityManager._ships[gameplayManager.activePlayerNr].cx));
-          if(Math.abs(targetx - entityManager._ships[gameplayManager.activePlayerIndex].cx) < 50){
+          if(Math.abs(targetx - ship.cx) < 50){
             console.log("dont shoot");
             //move and rotate instead
             destX += startVelX;
-            destX = util.clamp(destX);
-            path = this.AIMovement(path);
-            direction = this.AIrotation(direction);
-            this.shipUpdate(destX, path, direction);
+            destX = util.clamp(destX, ship);
+            path = this.AIMovement(path, ship);
+            //direction = this.AIrotation(direction, ship);
+            this.shipUpdate(destX, path, direction, ship);
             //change direction and run movement
           } else {
-            entityManager._ships[gameplayManager.activePlayerIndex].maybeFireBullet();
+            ship.maybeFireBullet();
+            this.timer = 1000;
           }
 
         } else {
           destX += startVelX;
-          destX = util.clamp(destX);
-          path = this.AIMovement(path);
-          direction = this.AIrotation(direction);
-          this.shipUpdate(destX, path, direction);
+          destX = util.clamp(destX, ship);
+          path = this.AIMovement(path, ship);
+        //  direction = this.AIrotation(direction, ship);
+          this.shipUpdate(destX, path, direction, ship);
 
     }
   },
 
-  shipUpdate: function(destX, path, direction){
+  shipUpdate: function(destX, path, direction, ship){
 
     //update all the variables in ship
-    entityManager._ships[gameplayManager.activePlayerIndex].destX = destX;
-    entityManager._ships[gameplayManager.activePlayerIndex].AIdirection = direction;
-    entityManager._ships[gameplayManager.activePlayerIndex].AIpath = path;
+    ship.destX = destX;
+    ship.AIdirection = direction;
+    ship.AIpath = path;
   }
 
 
